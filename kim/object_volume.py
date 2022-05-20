2aafd5efea42ea1aa2e229b27a203e017dd88b40
Kijun Kim <41587596+catumbra@users.noreply.github.com>
2022-05-20 15:15:26 +0900

    Update object_volume.py
    명칭변경: matarial -> object

--- kim/object_volume.py
+++ kim/object_volume.py
@@ -13,16 +13,16 @@ class objectsInImg:
         self.objList = list()
 
 #ref width는 100원의 지름인 24mm를 기본값으로
-def getTargetVolume(imgPath_topview, imgPath_sideview, refMatarial_width=24):
+def getTargetVolume(imgPath_topview, imgPath_sideview, refObject_width=24):
     """
     TODO: 코드 서순
     1. 탑뷰 이미지에서 발견한 물체들 나열
     2. 사이드뷰 이미지에서 발견한 물체들 나열
-    3. 1,2 에서 발견한 물체들 중 각각 referance matarial(refM)과 target matarial(tarM) 하나씩 선택
-    4. refM과 tarM의 top, side에서의 width, height 리스트에 저장
-    5. tarM의 top, side에서의 contour area(cArea)를 리스트에 저장
-    6. 4에서 구한 refM의 width들로 각도(camAngle_sin)를 계산
-    7. tarM_top의 height와 camAngle_sin으로 3차원 높이(3dHeight) 계산
+    3. 1,2 에서 발견한 물체들 중 각각 referance object(refO)과 target object(tarO) 하나씩 선택
+    4. refO과 tarO의 top, side에서의 width, height 리스트에 저장
+    5. tarO의 top, side에서의 contour area(cArea)를 리스트에 저장
+    6. 4에서 구한 refO의 width들로 각도(camAngle_sin)를 계산
+    7. tarO_top의 height와 camAngle_sin으로 3차원 높이(3dHeight) 계산
     8. 물체를 둘러싸는 큐브 부피 구하기
     9. cArea를 이용해서 top_proportion과 side_proportion구한 뒤 prism부피 계산
     10. 두개의 prism부피 평균값 구한 뒤 리턴
